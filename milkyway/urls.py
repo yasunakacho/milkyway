335bc1c51c6c3b8fe77961e7f918844877d5e239
yasunakacho <yasu1987@me.com>
2017-11-17 07:32:35 +0900

    created table and detailed page

--- milkyway/urls.py
+++ milkyway/urls.py
@@ -19,5 +19,6 @@ from web import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^crypto/(?P<id>[0-9]+)/$', views.detail, name='detail')
 ]
