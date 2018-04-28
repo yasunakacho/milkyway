b3b732a599c654c551cb92fac7b3a05d5dee6fcc
yasunakacho <yasu1987@me.com>
2018-04-28 22:39:36 +0900

    long time no push

--- milkyway/settings.py
+++ milkyway/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Replace database setting 
+# Replace database setting
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
